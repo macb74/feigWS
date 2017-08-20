d86eb29a91b8e9ba7783360c178b4929e6ef27af
martin <martin@meru>
2017-08-20 12:39:26 +0200

    OBIDISC4J in ein lokales maven repository aufgenommen

---
+++ installFeigLib.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+export JAVA_HOME=/opt/jdk1.8.0_144/
+MVN=/opt/maven/bin/mvn
+
+$MVN deploy:deploy-file -Dfile=./lib/OBIDISC4J.jar -DgroupId=de.feig -DartifactId=OBIDISC4J -Dversion=4.06.06 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+
