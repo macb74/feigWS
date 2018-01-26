5486405c680c56a968df2f6e0ceb047b116d789b
Martin <martin@martin-bussmann.de>
2018-01-26 16:01:41 +0100

    added RPI V4.8.0 to local mvn repository

--- installFeigLib.sh
+++ installFeigLib.sh
@@ -3,5 +3,7 @@
 export JAVA_HOME=/opt/jdk1.8.0_144/
 MVN=/opt/maven/bin/mvn
 
-$MVN deploy:deploy-file -Dfile=./lib/OBIDISC4J.jar -DgroupId=de.feig -DartifactId=OBIDISC4J -Dversion=4.8.1 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+$MVN deploy:deploy-file -Dfile=./lib/ID_ISC.SDK.Java-V4.8.1/sub-prj/lib/fedm-java/build/dist/OBIDISC4J.jar -DgroupId=de.feig -DartifactId=OBIDISC4J -Dversion=4.8.1 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
+
+$MVN deploy:deploy-file -Dfile=./lib/ID_ISC.SDK.Raspi-V4.8.0/sub-prj/lib/fedm-java/build/dist/OBIDISC4J.jar -DgroupId=de.feig -DartifactId=OBIDISC4J -Dversion=rpi4.8.0 -Dpackaging=jar -Durl=file:./maven-repository/ -DrepositoryId=maven-repository -DupdateReleaseInfo=true
 
